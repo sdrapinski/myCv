a913a7424a5126e0964ba3a535a9b84e796bcf63
sdrapinski <szymondrapinski@gmail.com>
2023-10-07 00:38:07 +0200

    project response

--- src/Pages/Projects/Projdisplay.jsx
+++ src/Pages/Projects/Projdisplay.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { Container,Row,Col } from 'react-bootstrap'
-import './Project.css'
-import ProjectPop from './ProjectPop'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Project.css";
+import ProjectPop from "./ProjectPop";
 
 const Projdisplay = () => {
-    return (
-        <Container id="projects" fluid style={{marginTop:"15vh"}}>
-            <Row style={{marginBottom:"15vh"}}>
-                <Col style={{textAlign:"center"}}> <h1>
-                    Projekty  </h1></Col>
-            </Row>
-            <Row className="projektyRow" >
-                <ProjectPop klasa="p1" nazwa="Projekt sklepu"/>
-                <ProjectPop klasa="p2" nazwa="Layout"/>
-              
-                
-            </Row>
-           
-        </Container>
-    )
-}
+  return (
+    <Container id="projects" fluid style={{ marginTop: "15vh" }}>
+      <Row style={{ marginBottom: "15vh" }}>
+        <Col style={{ textAlign: "center" }}>
+          {" "}
+          <h1>Projekty </h1>
+        </Col>
+      </Row>
+      <Row className="projektyRow">
+        <ProjectPop klasa="p1" nazwa="Projekt sklepu" />
+        <ProjectPop klasa="p1" nazwa="Projekt sklepu" />
+        <ProjectPop klasa="p2" nazwa="Layout" />
 
-export default Projdisplay
+        <ProjectPop klasa="p1" nazwa="Projekt sklepu" />
+        <ProjectPop klasa="p2" nazwa="Layout" />
+        <ProjectPop klasa="p2" nazwa="Layout" />
+      </Row>
+    </Container>
+  );
+};
+
+export default Projdisplay;
